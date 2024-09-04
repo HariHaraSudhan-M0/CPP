a2e3e967da85573b3bffe7228d460ee6a821fac6
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-09-04 12:23:14 +0530

    double datatype with decimal values

--- datatypes/datatypes.cpp
+++ datatypes/datatypes.cpp
@@ -4,5 +4,10 @@ int main()
     int age=10;
     int year=2023;
     int day=31;
-    std::cout<<year;
+    std::cout<<"year is :"<<year<<"\n";
+
+    double price=10.88;
+    double cgpa= 9.8;
+    double temperature = 27.3;
+    std::cout<<"cgpa is :"<<cgpa;
 }
